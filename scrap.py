32d76ca0fc4d502c9920ccf461d164200d66824d
mjdarvishi <mjdarvishi1374@gmail.com>
2022-05-29 15:55:51 +0430

    first commit

---
+++ scrap.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+import openpyxl
+
+filename = 'b:\\python\\sample.xlsx'
+wb = openpyxl.load_workbook(filename=filename)
+sheet = wb['Sheet1']
+for x in range(400,600,1):
+    # Getting page HTML
+    page = requests.get(f'https://divar.ir/s/tehran/auto?page={x}')
+    # Parsing content using beautifulsoup
+    soup = BeautifulSoup(page.content, 'html.parser')
+    links = soup.select("div .post-card-item")
+    for idx, item in enumerate(links):
+        details = item.select(
+            'div .kt-post-card__description')[0].text.split('\n')
+        new_row = [item.select('div .kt-post-card__title')[0].text,
+                   details[1] if len(details) > 1 else 0, details[0],(item.select('a', href=True)[0]['href']).split('/')[-1]]
+        sheet.append(new_row)
+    print(f'page {x} has been completed')   
+    wb.save(filename)
